fc76abbdc7f4a6243d773e669adc803abc3b4659
TatsunoriSeki <51009619+TatsunoriSeki@users.noreply.github.com>
2021-11-27 00:20:58 +0900

    アロー関数使い方

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import "./styles.css";
+// //テンプレート文字列
+// const name = "佐藤";
+// const age = 28;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// //従来の方法
+// const message1 = name + "です。" + age + "です。";
+// console.log(message1);
+
+// //テンプレート文字列
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+//アロー関数
+//従来の方法
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("test"));
+
+//単一式は波括弧やreturnを省略できる
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("test2"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(2, 3));
